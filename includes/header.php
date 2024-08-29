f3617330453e558d525271a79784722a3621cb4d
1BhavaniR <bb2040224@gmail.com>
2024-08-29 20:59:25 +0530

    font change

--- includes/header.php
+++ includes/header.php
@@ -79,7 +79,7 @@
 
 
  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="home.php">Mess Management</a>
+        <a class="navbar-brand" href="home.php">TPGIT DORMS</a>
         <!-- <img src="assets\images\hostels\logo1.jpeg"> -->
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
